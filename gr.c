586e4a339a9a5b12f6e2e30b2cf7f832110ab239
Steven Dee <steve@smartercode.net>
2014-07-08 14:54:22 -0700

    bellman-ford

--- gr.c
+++ gr.c
@@ -176,6 +176,7 @@ _gr_dfs_visit(gr_graph* g, size_t u, gr_list* acc)
   dfs_time = dfs_time + 1;
   g->vs[u].f = dfs_time;
   tmp->v = u;
+  tmp->w = -1;  // XX
   tmp->n = acc;
   return tmp;
 }
@@ -201,12 +202,91 @@ gr_dfs(gr_graph* g)
 }
 
 static void
-_dump_edge(const gr_list* l)
+_initialize_single_source(gr_graph* g, size_t s)
 {
-  printf("%lu", l->v);
+  size_t i;
+
+  for (i = 0; i < g->n; i++) {
+    g->vs[i].d.typ = typ_inf;
+    g->vs[i].d.v = g->n;
+  }
+  g->vs[s].d.typ = typ_num;
+  g->vs[s].d.v = 0;
+}
+
+static __inline__ int
+_inf_int_cmp(const inf_int_t* x, const inf_int_t* y, int w)
+{
+  if (x->typ == typ_inf) {
+    if ( y->typ == typ_inf) {
+      return 0;
+    }
+    else {
+      return 1;
+    }
+  }
+  else if (y->typ == typ_inf) {
+    return -1;
+  }
+  else if (x->v < y->v + w) {
+    return -1;
+  }
+  else if (x->v > y->v + w) {
+    return 1;
+  }
+  else return 0;
+}
+
+static void
+_relax(gr_graph* g, size_t u, gr_list* l)
+{
+  inf_int_t* ud;
+  inf_int_t* vd;
+  int        w = l->w;
+
+  assert(u < g->n);
+
+  ud = &g->vs[u].d;
+  vd = &g->vs[l->v].d;
+  if (_inf_int_cmp(vd, ud, w) == 1) {
+    vd->typ = typ_num;
+    vd->v = ud->v + w;
+    g->vs[l->v].p = u;
+  }
+}
+
+int
+gr_bellman_ford(gr_graph* g, size_t s)
+{
+  size_t   i;
+  size_t   j;
+  gr_list* l;
+
+  _initialize_single_source(g, s);
+  for (i = 0; i < g->n - 1; i++) {
+    for (j = 0; j < g->n; j++) {
+      for (l = g->es[j]; l; l = l->n) {
+        _relax(g, j, l);
+      }
+    }
+  }
+  for (j = 0; j < g->n; j++) {
+    for (l = g->es[j]; l; l = l->n) {
+      if (_inf_int_cmp(&g->vs[l->v].d, &g->vs[j].d, l->w) == 1) {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+static void
+_dump_edge(const gr_graph* g, const gr_list* l)
+{
+  printf("%lu (w:%d p:%lu)", l->v, l->w, g->vs[l->v].p);
   if (l->n) {
     printf(", ");
-    _dump_edge(l->n);
+    _dump_edge(g, l->n);
   }
 }
 
@@ -219,7 +299,7 @@ gr_dump_graph(const gr_graph* g)
   for (i = 0; i < g->n; i++) {
     if (g->es[i]) {
       printf("  %lu, {", i);
-      _dump_edge(g->es[i]);
+      _dump_edge(g, g->es[i]);
       printf("}\n");
     }
   }
@@ -242,20 +322,20 @@ _gr_add_all(gr_graph* g, const edge_t* es, size_t len)
 }
 
 static const edge_t edges[] = {
-  {0,  1,  1},
-  {1,  2,  1},
-  {2,  3,  1},
-  {0,  2,  1},
+  {0,  1,  4},
+  {1,  2,  8},
+  {2,  3,  2},
+  {0,  2,  30},
   {5,  6,  1},
-  {7,  5,  1},
-  {6,  8,  1},
-  {8,  1,  1},
-  {9,  1,  1},
-  {10, 1,  1},
-  {13, 10, 1},
-  {13, 9,  1},
-  {13, 8,  1},
-  {3,  13, 1}
+  {7,  5,  15},
+  {6,  8,  7},
+  {8,  1,  6},
+  {9,  1,  4},
+  {10, 1,  6},
+  {13, 10, 90},
+  {13, 9,  2},
+  {13, 8,  4},
+  {3,  13, 6}
 };
 
 int
@@ -267,10 +347,10 @@ main()
 
   {
     gr_bfs(g, 0);
-    print_path(g, 0, 1); puts("");
-    print_path(g, 0, 2); puts("");
-    print_path(g, 0, 3); puts("");
     print_path(g, 0, 13); puts("");
+    print_path(g, 0, 4); puts("");
+    print_path(g, 0, 3); puts("");
+    print_path(g, 0, 10); puts("");
   }
 
   {
@@ -284,10 +364,25 @@ main()
     gr_list* topsort = gr_dfs(g);
 
     puts("topological order:");
-    _dump_edge(topsort);
+    _dump_edge(g, topsort);
     puts("");
     puts("");
     _gr_list_free(topsort);
+  }
+
+  {
+    int r = gr_bellman_ford(g, 0);
+
+    if (r) {
+      printf("no negative weight cycles\n");
+      print_path(g, 0, 13); puts("");
+      print_path(g, 0, 4); puts("");
+      print_path(g, 0, 3); puts("");
+      print_path(g, 0, 10); puts("");
+    }
+    else {
+      printf("negative weight cycle detected\n");
+    }
   }
 
   gr_dump_graph(g);
